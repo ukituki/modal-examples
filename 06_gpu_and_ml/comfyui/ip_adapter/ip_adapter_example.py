c64a2aacbee89ae6c915ecd2f4a7f056ce43198a
ukituki <ukituki@gmail.com>
2025-04-27 10:56:00 +0200

    fix(ip_adapter_example): update comfy-cli version and modify node installation command

--- 06_gpu_and_ml/comfyui/ip_adapter/ip_adapter_example.py
+++ 06_gpu_and_ml/comfyui/ip_adapter/ip_adapter_example.py
@@ -11,12 +11,13 @@ image = (  # build up a Modal Image to run ComfyUI, step by step
         python_version="3.11"
     )
     .apt_install("git")  # install git to clone ComfyUI
-    .pip_install("comfy-cli==1.2.7")  # install comfy-cli
+    .pip_install("comfy-cli==1.3.8")  # install comfy-cli
     .run_commands(  # use comfy-cli to install the ComfyUI repo and its dependencies
         "comfy --skip-prompt install --nvidia"
     )
     .run_commands(  # download the WAS Node Suite custom node pack
-        "comfy node install ComfyUI_IPAdapter_plus"
+        # "comfy node install ComfyUI_IPAdapter_plus"
+        "comfy node registry-install comfyui_ipadapter_plus"
     )
     .run_commands("apt install -y wget")
     .run_commands(  # the Unified Model Loader node requires these two models to be named a specific way, so we use wget instead of the usual comfy model download command
